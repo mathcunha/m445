0c964e94ca2c93fec81667b06940e3c4123e835e
mathcunha <mathcunha@be0732a4-09e3-11de-b5bf-434079a3519f>
2009-03-06 01:27:22 +0000

    deletando tudo

--- trunk/src/arcademis/RequestSender.java
+++
@@ -1,7 +0,0 @@
-package arcademis;
-
-/**
- * This class defines a request sender. This service handler is responsible for
- * transmitting from the stub to the skeleton a remote method invocation.
- */
-public class RequestSender extends ProtocolHandler {}
