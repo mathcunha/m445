e0fc56d9a700bb8778d677d7033dbdfd45be35c2
mathcunha <mathcunha@be0732a4-09e3-11de-b5bf-434079a3519f>
2009-03-12 01:04:28 +0000

    App teste

---
+++ trunk/src/br/unifor/agenda/server/AgendaServer.java
@@ -0,0 +1,60 @@
+package br.unifor.agenda.server;
+
+import rme.RmeConfigurator;
+import rme.naming.RmeServiceDirectory_Impl;
+import arcademis.server.AlreadyBoundException;
+import br.unifor.agenda.PhoneBook;
+import br.unifor.agenda.PhoneBook_Stub;
+
+public class AgendaServer {
+	/*public static void main(String args[]) {
+		try {
+			rme.RmeConfigurator c = new rme.RmeConfigurator();
+			c.configure();
+			PhoneBook o = new PhoneBook();
+			rme.naming.RmeNaming.bind("obj", o);
+			o.activate();
+
+			o.insert("Matheus", "Marcos Macêdo", "Aldeota");
+
+			o.insert("Fulano", " Macêdo Marcos", "Varjota");
+			
+			RmeServiceDirectory_Impl nameService = new RmeServiceDirectory_Impl();
+
+			nameService.activate();
+		} catch (arcademis.ArcademisException e) {
+		} catch (arcademis.concreteComponents.MalformedURLException e) {
+		} catch (AlreadyBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}*/
+	
+	public static void main(String[] args)
+	{
+        try
+        {
+			RmeConfigurator c = new RmeConfigurator();
+			c.configure();
+			
+			RmeServiceDirectory_Impl nameService = new RmeServiceDirectory_Impl();
+
+			nameService.activate();
+			
+			PhoneBook o = new PhoneBook();			
+			o.insert("Matheus", "Marcos Macêdo", "Aldeota");
+			o.insert("Fulano", " Macêdo Marcos", "Varjota");
+			rme.naming.RmeNaming.bind("obj", o);
+			o.activate();
+
+			
+			
+			//nameService.bind("obj", new PhoneBook_Stub());
+        }
+        catch (Exception e)
+        {
+			e.printStackTrace();
+        }
+	}
+}
