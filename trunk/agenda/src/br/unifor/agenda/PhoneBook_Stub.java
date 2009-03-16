1eb6928e07fdf2975c6b17d7d1c8cdc16fb48579
robertoclj <robertoclj@be0732a4-09e3-11de-b5bf-434079a3519f>
2009-03-16 23:00:37 +0000

    Stub alterado para refazer o lookup e deixar o objeto funcionando mesmo que a referencia para o server tenha caído ou o objeto migrado.

--- trunk/agenda/src/br/unifor/agenda/PhoneBook_Stub.java
+++ trunk/agenda/src/br/unifor/agenda/PhoneBook_Stub.java
@@ -1,33 +1,66 @@
 package br.unifor.agenda;
 
-import br.unifor.mobile.MobileStub;
-import arcademis.MarshalException;
-import arcademis.NetworkException;
-import arcademis.Stream;
-import rme.MultiServerStub;
+import rme.RmeConfigurator;
 import rme.RmeStream;
 import rme.RmeStreamFc;
+import rme.naming.RmeNaming;
+import arcademis.ArcademisException;
+import arcademis.MarshalException;
+import arcademis.NetworkException;
+import arcademis.NotBoundException;
+import arcademis.ReconfigurationException;
+import arcademis.Stream;
+import arcademis.concreteComponents.MalformedURLException;
+import br.unifor.mobile.MobileStub;
 
 public class PhoneBook_Stub extends MobileStub {
 
+	PhoneBook_Stub stub = this;
+
 	public Pessoa getPhoneAddress(String name) throws NetworkException {
-		
+		return stub.getPhoneAddress_stub(name);
+	}
+
+	public Pessoa getPhoneAddress_stub(String name) throws NetworkException {
+
 		Pessoa lPessoa = null;
 		Stream stream = (new RmeStreamFc()).createStream();
 		try {
 			stream.write("getPhoneAddress");
 			stream.write(1);
 			stream.write(name);
-			
-			RmeStream lStream = (RmeStream)super.invoke(stream, 1, '?', 0);
+
+			RmeStream lStream = (RmeStream) super.invoke(stream, 1, '?', 0);
 			lPessoa = new Pessoa();
 			lPessoa.unmarshal(lStream);
-			
+
 		} catch (MarshalException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (Exception e) {
+
+			RmeConfigurator c = new RmeConfigurator();
+			try {
+				c.configure();
+				stub = (PhoneBook_Stub) RmeNaming.lookup("obj");
+				return stub.getPhoneAddress(name);
+
+			} catch (ReconfigurationException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (MalformedURLException ex) {
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			} catch (ArcademisException ex) {
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			} catch (NotBoundException ex) {
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			}
+
 		}
-		
+
 		return lPessoa;
 	}
 }
