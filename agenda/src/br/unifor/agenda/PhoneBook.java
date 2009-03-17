c0c68b09f8a80f67450f9f9598c8acdc3472c5d7
robertoclj <robertoclj@be0732a4-09e3-11de-b5bf-434079a3519f>
2009-03-17 13:05:38 +0000

    Imprime o nome da máquina na chamada do getAddress (HostName)

--- agenda/src/br/unifor/agenda/PhoneBook.java
+++ agenda/src/br/unifor/agenda/PhoneBook.java
@@ -1,5 +1,7 @@
 package br.unifor.agenda;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Hashtable;
 import java.util.Set;
 import java.util.Map.Entry;
@@ -10,44 +12,47 @@ import arcademis.MarshalException;
 import arcademis.Stream;
 import br.unifor.mobile.MobileRemoteObject;
 
-
-
-public class PhoneBook extends MobileRemoteObject implements
-		PhoneCatalogue {
+public class PhoneBook extends MobileRemoteObject implements PhoneCatalogue {
 
 	Hashtable<String, Pessoa> h = new Hashtable();
 
 	public Pessoa getPhoneAddress(String name) {
-		System.out.println("name "+name);
+		try {
+			System.out.println("[PhoneBook] - "
+					+ InetAddress.getLocalHost().getHostName() + " nome "
+					+ name);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 		return (Pessoa) h.get(name);
 	}
 
 	public void insert(String name, String rua, String bairro) {
 		Pessoa lp = new Pessoa();
 		lp.setName(name);
-		
+
 		Endereco endereco = new Endereco();
 		endereco.setBairro(bairro);
 		endereco.setRua(rua);
-		
+
 		lp.setEndereco(endereco);
-		
+
 		h.put(name, lp);
 	}
 
 	@Override
 	public void getState(Stream stream) {
-		RmeStream lStream = (RmeStream)stream;
+		RmeStream lStream = (RmeStream) stream;
 		Set<Entry<String, Pessoa>> valores = h.entrySet();
-		
+
 		try {
 			stream.write(valores.size());
-			System.out.println("valores.size() "+valores.size());
+			System.out.println("valores.size() " + valores.size());
 			for (Entry<String, Pessoa> entry : valores) {
 				stream.write(entry.getKey());
 				entry.getValue().marshal(stream);
 			}
-			
+
 		} catch (MarshalException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -57,24 +62,24 @@ public class PhoneBook extends MobileRemoteObject implements
 	@Override
 	public void restoreState(Stream stream) {
 		h = new Hashtable<String, Pessoa>();
-		
+
 		RmeStream lStream = (RmeStream) stream;
 		try {
 			int tamanho = stream.readInt();
 			String chave;
 			Pessoa pessoa;
-			System.out.println("tamanho "+tamanho);
+			System.out.println("tamanho " + tamanho);
 			for (int i = 0; i < tamanho; i++) {
-				chave= (String)lStream.readObject();
+				chave = (String) lStream.readObject();
 				pessoa = new Pessoa();
 				pessoa.unmarshal(stream);
-				
-				System.out.println("chave "+chave);
-				System.out.println("pessoa "+pessoa);
-				
+
+				System.out.println("chave " + chave);
+				System.out.println("pessoa " + pessoa);
+
 				h.put(chave, pessoa);
 			}
-			
+
 		} catch (MarshalException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
