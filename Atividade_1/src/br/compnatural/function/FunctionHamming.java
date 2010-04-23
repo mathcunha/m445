c1a3142baeb0fea87a297de1a52b9ceb8cabc628
mathcunha <mathcunha@be0732a4-09e3-11de-b5bf-434079a3519f>
2010-04-23 00:49:02 +0000

    Finalizando a primeira questão da atividade 2

--- Atividade_1/src/br/compnatural/function/FunctionHamming.java
+++ Atividade_1/src/br/compnatural/function/FunctionHamming.java
@@ -1,22 +1,108 @@
 package br.compnatural.function;
 
 import br.compnatural.State;
+import br.compnatural.coordinate.BinaryCoordinate;
 
 public class FunctionHamming implements MathFunction {
 	
 	private State g;
 	final Boolean max;
 	
-	public FunctionHamming (Boolean max){
+	private static final boolean ARRAY_4_3[] = new boolean[]{false, true, false,
+			false, true, false,
+			false, true, false,
+			false, true, false};
+	
+	private static final boolean ARRAY_12_9[] = new boolean[]{false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false,
+			false, false, false, true, true, true, false, false, false
+			};
+	
+	private static final boolean ARRAY_40_30[] = new boolean[]{false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false,
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false,
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false,
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, 
+			false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false
+			};
+	
+	public FunctionHamming (Boolean max, int lenght){
+		if(!max){
+			throw new UnsupportedOperationException("Tem que rever esta implementação");			
+		}
 		this.max = max;
 		g = State.getState();
-		g.setValue(0d);
+		g.setValue((double)lenght);
 	}
 
 	@Override
 	public Double eval(State state) {
-		// TODO Auto-generated method stub
-		return null;
+		int count = 0 ;
+		BinaryCoordinate coordinate = (BinaryCoordinate)state.getCoordinate().get(0);
+		boolean[] array = null;
+		switch (coordinate.getValue().length) {
+		case 12:
+			array = ARRAY_4_3;
+			break;
+		case 108:
+			array = ARRAY_12_9;
+			break;
+		case 1200:
+			array = ARRAY_40_30;
+			break;
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			if(coordinate.getValue()[i] != array[i]){
+				count++;
+			}
+		}
+		Double ret = (new Integer(coordinate.getValue().length - count)).doubleValue();		
+		
+		return  ret;
 	}
 
 	@Override
