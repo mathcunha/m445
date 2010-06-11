30d22c517f474c72772f0354cb6645c3a1f03648
mathcunha <mathcunha@be0732a4-09e3-11de-b5bf-434079a3519f>
2010-06-11 19:47:51 +0000

    Refazendo a multilayer

---
+++ trunk/MATHCUNHA_COMPNATURAL/src/br/compnatural/rna/network/MultilayerPerceptronNew.java
@@ -0,0 +1,177 @@
+package br.compnatural.rna.network;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import Jama.Matrix;
+import br.compnatural.rna.Layer;
+import br.compnatural.rna.Neuron;
+import br.compnatural.rna.Pattern;
+import br.compnatural.rna.Util;
+import br.compnatural.rna.funtion.RnaFunction;
+import br.compnatural.rna.funtion.TangenteHiperbolica;
+
+public class MultilayerPerceptronNew {
+	private double minWeight;
+	private double maxWeight;
+	private List<Layer> layers;
+
+	protected Logger log = Logger.getLogger(MultilayerPerceptronNew.class
+			.getName());
+
+	public MultilayerPerceptronNew(double minWeight, double maxWeight) {
+		this.maxWeight = maxWeight;
+		this.minWeight = minWeight;
+	}
+
+	public void backprop(int max_it, double min_error, double alfa,
+			Pattern pattern, int numNeuronsHidden) {
+		MultilayerPerceptronNew lMultilayerPerceptron = getMultilayerPerceptronTangenteOneHidden(
+				numNeuronsHidden, pattern.getD().length,
+				pattern.getX()[0].length, minWeight, maxWeight);
+		Random random = new Random(System.currentTimeMillis());
+
+		for (int i = 0; i < max_it; i++) {
+
+			int index = 0;
+			Set<Integer> indexes = new LinkedHashSet<Integer>(
+					pattern.getD().length);
+
+			while (indexes.size() < pattern.getD().length) {
+
+				int aux = indexes.size();
+				index = random.nextInt(pattern.getD().length);
+				indexes.add(index);
+				if (aux == indexes.size()) {
+					continue;
+				}
+				
+				Matrix[] Y = new Matrix[lMultilayerPerceptron.getLayers().size()+1];
+				Matrix[] sensitivity = new Matrix[lMultilayerPerceptron.getLayers().size()];
+				
+				Y[0] = pattern.getXMatrix()[index];
+				
+				for (int j = 1; j < Y.length; j++) {
+					Layer layer = lMultilayerPerceptron.getLayers().get(j-1);					
+					Y[j] = new Matrix(layer.run(Y[j-1].getColumnPackedCopy())); 
+				}
+				
+				Matrix dMtr = pattern.getDMatrix()[index];
+				Matrix eMtr = dMtr.minus(Y[Y.length -1]);
+				sensitivity[sensitivity.length -1] = getF(lMultilayerPerceptron.getLayers().get(sensitivity.length -1)).times(-2);
+				sensitivity[sensitivity.length -1].times(eMtr);
+				
+				for (int j = sensitivity.length-2; j >= 0; j--) {
+					sensitivity[j] = getF(lMultilayerPerceptron.getLayers().get(j)).times(lMultilayerPerceptron.getLayers().get(j+1).getW().transpose());
+					sensitivity[j] = sensitivity[j].times(sensitivity[j+1]);
+				}
+				
+				for (int j = 0; j < sensitivity.length; j++) {
+					Layer layer = lMultilayerPerceptron.getLayers().get(j);
+					layer.minusDeltaW(sensitivity[j].times(alfa).times(Y[j].transpose()));
+					layer.minusDeltaB(sensitivity[j].times(alfa));
+				}
+				
+			}
+			log.fine(indexes.toString());
+		}
+
+		this.setLayers(lMultilayerPerceptron.getLayers());
+	}
+	
+	public double[][] run(Pattern pattern, int index){
+		Matrix[] Y = new Matrix[getLayers().size()+1];
+		for (int j = 1; j < Y.length; j++) {
+			Layer layer = getLayers().get(j-1);					
+			Y[j] = new Matrix(layer.run(Y[j-1].getColumnPackedCopy())); 
+		}
+		
+		return Y[Y.length-1].getArray();
+	}
+	
+	public Matrix getF(Layer layer) {
+		double[][] matrix = new double[layer.getNeurons().size()][layer
+				.getNeurons().size()];
+		for (int i = 0; i < matrix.length; i++) {
+			Neuron neuron = layer.getNeurons().get(i);
+			matrix[i][i] = neuron.getFunction().eval_derivative(neuron.getU());
+		}
+		return new Matrix(matrix);
+	}
+
+	public static MultilayerPerceptronNew getMultilayerPerceptronTangenteOneHidden(
+			int numNeuronsHidden, int numNeuronsOut, int weights,
+			double minWeight, double maxWeight) {
+		Random lRandom = new Random(System.currentTimeMillis());
+		MultilayerPerceptronNew retorno = new MultilayerPerceptronNew(
+				minWeight, maxWeight);
+		retorno.setLayers(new ArrayList<Layer>(2));
+		retorno.getLayers().add(
+				new Layer(new ArrayList<Neuron>(numNeuronsHidden), minWeight,
+						maxWeight));
+		retorno.getLayers().add(
+				new Layer(new ArrayList<Neuron>(numNeuronsOut), minWeight,
+						maxWeight));
+
+		mountLayer(numNeuronsHidden, retorno.getLayers().get(0).getNeurons(),
+				weights, lRandom, minWeight, maxWeight,
+				TangenteHiperbolica.class);
+
+		mountLayer(numNeuronsOut, retorno.getLayers().get(1).getNeurons(),
+				numNeuronsHidden, lRandom, minWeight, maxWeight,
+				TangenteHiperbolica.class);
+
+		return retorno;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void mountLayer(int lenNeuron, List<Neuron> neurons,
+			int weights, Random random, double minWeight, double maxWeight,
+			Class functionClass) {
+		for (int i = 0; i < lenNeuron; i++) {
+			double[] weigth = new double[weights];
+
+			for (int j = 0; j < weights; j++) {
+				weigth[j] = randomValueInRange(random, minWeight, maxWeight);
+			}
+			Neuron neuron = new Neuron(weigth, randomValueInRange(random,
+					minWeight, maxWeight), (RnaFunction) Util
+					.createObject(functionClass.getName()));
+			neuron.setBias(randomValueInRange(random, minWeight, maxWeight));
+			neurons.add(neuron);
+		}
+	}
+
+	public double getMinWeight() {
+		return minWeight;
+	}
+
+	public void setMinWeight(double minWeight) {
+		this.minWeight = minWeight;
+	}
+
+	public double getMaxWeight() {
+		return maxWeight;
+	}
+
+	public void setMaxWeight(double maxWeight) {
+		this.maxWeight = maxWeight;
+	}
+
+	public List<Layer> getLayers() {
+		return layers;
+	}
+
+	public void setLayers(List<Layer> layers) {
+		this.layers = layers;
+	}
+
+	public static double randomValueInRange(Random random, double min,
+			double max) {
+		return Util.randomValueInRange(random, min, max);
+	}
+}
