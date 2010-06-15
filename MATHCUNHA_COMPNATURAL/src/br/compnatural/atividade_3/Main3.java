7ad3813450c4072ec226bc474ca70bead7d8ad42
mathcunha <mathcunha@be0732a4-09e3-11de-b5bf-434079a3519f>
2010-06-15 01:06:42 +0000

    Fim!

--- MATHCUNHA_COMPNATURAL/src/br/compnatural/atividade_3/Main3.java
+++ MATHCUNHA_COMPNATURAL/src/br/compnatural/atividade_3/Main3.java
@@ -2,18 +2,20 @@ package br.compnatural.atividade_3;
 
 public class Main3 {
 	public static void main(String[] args) {
-		if("1".equals(args[0])){
-			Experimento_1 teste = new Experimento_1();
-			teste.run();
-		}else if("2".equals(args[0])){
-			Experimento_3 teste = new Experimento_3();
-			teste.run();
-		}else if("3".equals(args[0])){
-			Experimento_2GA teste = new Experimento_2GA();
-			teste.run();
-		}else if("4".equals(args[0])){
-			Experimento_2PSO teste = new Experimento_2PSO();
-			teste.run();
+		for (String arg : args) {
+			if("1".equals(arg)){
+				Experimento_1 teste = new Experimento_1();
+				teste.run();
+			}else if("2".equals(arg)){
+				Experimento_3 teste = new Experimento_3();
+				teste.run();
+			}else if("3".equals(arg)){
+				Experimento_2GA teste = new Experimento_2GA();
+				teste.run();
+			}else if("4".equals(arg)){
+				Experimento_2PSO teste = new Experimento_2PSO();
+				teste.run();
+			}
 		}
 	}
 }
